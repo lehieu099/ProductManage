178658a215205abaf01fa87fa9a07907c14eb41e
lehieu099 <duyhieu099a2@gmail.com>
2021-11-08 00:07:00 +0700

    pagination

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { Product } from '../ProductWorkSpace/product.model';
+import { Product } from '../pages/ProductWorkspace/product.model';
 import { catchError } from 'rxjs/operators';
 
 const apiUrl = 'https://61829a8e02f60a001775cdd4.mockapi.io/Product';
@@ -22,6 +22,10 @@ export class ProductService {
     return this.http.get<Product[] >(`${apiUrl}`, { params })
     .pipe(catchError(() => of([])));
   };
+
+  delProduct(id:number): Observable <Product[]>{
+    return this.http.delete<Product []>(apiUrl + '/' +id)
+  }
 
   constructor(private http: HttpClient) { }
 

a1b9ccbfbe128a0fc9003524804cf71ab3f3c66f
lehieu099 <duyhieu099a2@gmail.com>
2021-11-07 21:06:41 +0700

    .

---
+++ src/app/service/product.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { Product } from '../ProductWorkSpace/product.model';
+import { catchError } from 'rxjs/operators';
+
+const apiUrl = 'https://61829a8e02f60a001775cdd4.mockapi.io/Product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  getProduct(
+    pageIndex: number,
+    pageSize: number
+  ): Observable<Product[] > {
+    let params = new HttpParams()
+      .append('page', `${pageIndex}`)
+      .append('limit', `${pageSize}`);
+
+    return this.http.get<Product[] >(`${apiUrl}`, { params })
+    .pipe(catchError(() => of([])));
+  };
+
+  constructor(private http: HttpClient) { }
+
+}
